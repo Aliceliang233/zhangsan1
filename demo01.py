0bba7c9e17649b56985b7a4288361512b82211d6
Aliceliang233 <760464772@qq.com>
2020-09-19 20:17:41 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,17 @@
+"""
+print("你好")
+print("呱呱"*100)
+"""
+
+# a=float(input("请输入："))
+# b=float(input("请输入："))
+# print("内容：",a+b)
+
+# print(type(9.8))
+# print(type(()))
+# print(type({}))
+# print(type([]))
+
+a=input("请输入：")
+b=input("请输入：")
+print(len(a)+len(b))
